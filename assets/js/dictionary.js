9494445ccb2101ffd407902931862575d54beb3c
jshor <truthsofwolf@gmail.com>
2014-04-29 23:40:29 -0400

    cleaned out broken code

--- assets/js/dictionary.js
+++ assets/js/dictionary.js
@@ -1,26 +1,3 @@
-var videoPlayer;
-
-$(function() {
-	// set height of multimedia panel to fit screen
-	var multimediaHeight = screen.height
-		- $("div[data-role=header]").height()
-		- $("#multiplechoicePanel").height();
-	$("#multimediaPanel").height(multimediaHeight);
-
-		$("#multiplechoicePanel").show();
-
-	// resize the height of the media container to be a square
-	$(".media-container").height($(".media-container").width());
+var Dictionary = {
 	
-	// position the search bar at top
-	$("#").css("top", parseInt($("div[data-role=header]").height()));
-
-	// show caption if defined
-	if(caption != undefined)
-		$(".caption").html(caption).show();
-
-	// set up dialog
-	$("a[data-rel=dialog]").click(function() {
-		$("#dialog").dialog();
-	});
-});
+};
