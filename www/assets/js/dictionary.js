13af90d7b17ab6e8bb56ee58135f9c258b3325e7
Chris Sakai <cs.chris.sakai@gmail.com>
2014-05-05 00:59:30 -0400

    copy over files into other thing

--- www/assets/js/dictionary.js
+++ www/assets/js/dictionary.js
@@ -4,23 +4,26 @@ $(document).on('pageshow', '#dictionarypage', function() {
     for (i=0; i<data.entries.length; i++) {
       var classname="ui-li-static ui-body-inherit";
       console.log(data.entries[i].english);
+      console.log(typeof(data.entries[i].english));
       if (i==0) {
         classname+=" ui-first-child";
       }else if (i==data.entries.length-1) {
         classname+=" ui-last-child";
       }
       var h3=$("<h3></h3>").html(data.entries[i].kiswahili);
-      var pstrong=$("<p><strong></strong></p>").html(data.entries[i].english);
+      var pstrong=$("<p></p>").append("<strong></strong>").html(data.entries[i].english);
       console.log("test");
-      var p=null;
-      if (typeof(data.entries[i].notes)!=='undefined'){
-        p=$("<p></p>").html(data.entries[i].notes);
-      }
+
       var audio=$("<p></p>").addClass("ui-li-aside")
-      var link=$("<a></a>").attr("src", data.entries[i].audio);
-      audio.html("<strong><a>"+link+"</a></strong>");
+      var link=$("<a></a>").attr("href", "assets/audio/greetings/"+data.entries[i].audio);
+      link.html("audio");
+      audio.append($("<strong></strong>").append(link));
       var li=$("<li></li>");
-      li.html(h3+pstrong+p+audio);
+      li.append(h3).append(pstrong).append(audio);
+      if (typeof(data.entries[i].notes)!=='undefined'){
+        var p=$("<p></p>").html(data.entries[i].notes);
+        li.append(p);
+      }
       li.addClass(classname);
       li.attr("id", data.entries[i].english);
       $("#dictionary").append(li);
