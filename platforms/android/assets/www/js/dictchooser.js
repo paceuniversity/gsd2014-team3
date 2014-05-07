a405a694a37e01e15b6e0281f35ef467b03ad2d6
Chris Sakai <cs.chris.sakai@gmail.com>
2014-05-06 20:08:54 -0400

    implement dictionary sorting

--- platforms/android/assets/www/js/dictchooser.js
+++ platforms/android/assets/www/js/dictchooser.js
@@ -1,7 +1,5 @@
 $(document).on('pageshow', '#dict-chooser', function() {
   $.getJSON("data/master.json", function(data) {
-    console.log("Debug");
-    console.log(data.unlockedDictionaries);
     for (i=0; i<data.unlockedDictionaries.length; i++) {
       var classname="ui-li-static ui-body-inherit";
       if (i==0) {
@@ -12,7 +10,6 @@ $(document).on('pageshow', '#dict-chooser', function() {
       }
       var h3=$("<h3></h3>").html(data.unlockedDictionaries[i].Category);
       var link=$("<a></a>").attr("href", "data/dictionaries/"+data.unlockedDictionaries[i].File);
-      console.log(link);
       var li=$("<li></li>");
       li.append(h3);
       li.addClass(classname);
--- platforms/android/assets/www/js/dictionary.js
+++ platforms/android/assets/www/js/dictionary.js
@@ -1,33 +1,76 @@
+var dicentries=[];
 $(document).on('pageshow', '#dictionary', function() {
 console.log("Dictionary loaded");
   $.getJSON("data/dictionary/"+option, function(data) {/*you would put a variable here once we fix the chooser*/
     console.log(data.category);
     for (i=0; i<data.entries.length; i++) {
+      dicentries.push(data.entries[i]);
+    }
+    makelist();
+  });
+  $("#english").click(function(){
+    console.log("english sorting test");
+    console.log(dicentries);
+    dicentries.sort(function (a, b) {
+      if (a.english.toUpperCase() > b.english.toUpperCase())
+        return 1;
+      if (a.english.toUpperCase() < b.english.toUpperCase())
+        return -1;
+      // a must be equal to b
+      return 0;
+    });
+    console.log(dicentries);
+    $("#dictionarylist").html("");
+    makelist();
+  });
+  $("#kiswahili").click(function(){
+    dicentries.sort(function (a, b) {
+      if (a.kiswahili.toUpperCase() > b.kiswahili.toUpperCase())
+        return 1;
+      if (a.kiswahili.toUpperCase() < b.kiswahili.toUpperCase())
+        return -1;
+      // a must be equal to b
+      return 0;
+    });
+    console.log(dicentries);
+    $("#dictionarylist").html("");
+    makelist();
+  });
+  $("#homelink").click(function(){
+    dicentries=[];
+    console.log(dicentries);
+  });
+  function makelist(){
+    for (i=0; i<dicentries.length; i++) {
       var classname="ui-li-static ui-body-inherit";
-      console.log(data.entries[i].english);
-      console.log(typeof(data.entries[i].english));
       if (i==0) {
         classname+=" ui-first-child";
-      }else if (i==data.entries.length-1) {
+      }else if (i==dicentries.length-1) {
         classname+=" ui-last-child";
       }
-      var h3=$("<h3></h3>").html(data.entries[i].kiswahili);
-      var pstrong=$("<p></p>").append("<strong></strong>").html(data.entries[i].english);
-      console.log("test");
+      var h3=$("<h3></h3>").html(dicentries[i].kiswahili);
+      var pstrong=$("<p></p>").append("<strong></strong>").html(dicentries[i].english);
 
-      var audio=$("<p></p>").addClass("ui-li-aside")
-      var link=$("<a></a>").attr("href", "assets/audio/greetings/"+data.entries[i].audio);
-      link.html("audio");
-      audio.append($("<strong></strong>").append(link));
+      var audiv=$("<a></a>");
+      audiv.addClass("ui-btn-right jqm-home audio");
+      audiv.attr("data-filename", "assets/audio/greetings/"+dicentries[i].audio);
+      audiv.html("audio");
       var li=$("<li></li>");
-      li.append(h3).append(pstrong).append(audio);
-      if (typeof(data.entries[i].notes)!=='undefined'){
-        var p=$("<p></p>").html(data.entries[i].notes);
-        li.append(p);
-      }
+      li.append(h3).append(pstrong).append(audiv);
+      var p = $("<span></span>");
+      if (typeof(dicentries[i].notes))
+        p.html(dicentries[i].notes);
+      else
+        p.html("No additional notes");
+      p.hide();
+      li.append(p);
+      li.click(function(){
+        $("#note").html($(this).find("span").html());
+        $("#notes").popup("open");
+      });
       li.addClass(classname);
-      li.attr("id", data.entries[i].english);
+      li.attr("id", dicentries[i].english);
       $("#dictionarylist").append(li);
     }
-  });
+  }
 });
