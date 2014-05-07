0529a66f2e2f577d66262d37cae14d5b2e4ce31f
Chris Sakai <cs.chris.sakai@gmail.com>
2014-05-06 21:41:07 -0400

    add filter functionality to dictionary as well as changing order of english to kiswahili when sort by is changed

--- platforms/android/assets/www/js/dictionary.js
+++ platforms/android/assets/www/js/dictionary.js
@@ -1,16 +1,14 @@
 var dicentries=[];
+var isEnglish = false;
+
 $(document).on('pageshow', '#dictionary', function() {
-console.log("Dictionary loaded");
   $.getJSON("data/dictionary/"+option, function(data) {/*you would put a variable here once we fix the chooser*/
-    console.log(data.category);
     for (i=0; i<data.entries.length; i++) {
       dicentries.push(data.entries[i]);
     }
-    makelist();
+    makelist(dicentries);
   });
   $("#english").click(function(){
-    console.log("english sorting test");
-    console.log(dicentries);
     dicentries.sort(function (a, b) {
       if (a.english.toUpperCase() > b.english.toUpperCase())
         return 1;
@@ -19,9 +17,8 @@ console.log("Dictionary loaded");
       // a must be equal to b
       return 0;
     });
-    console.log(dicentries);
-    $("#dictionarylist").html("");
-    makelist();
+    isEnglish = true;
+    makelist(dicentries);
   });
   $("#kiswahili").click(function(){
     dicentries.sort(function (a, b) {
@@ -32,34 +29,37 @@ console.log("Dictionary loaded");
       // a must be equal to b
       return 0;
     });
-    console.log(dicentries);
-    $("#dictionarylist").html("");
-    makelist();
+    isEnglish = false;
+    makelist(dicentries);
   });
   $("#homelink").click(function(){
     dicentries=[];
-    console.log(dicentries);
   });
-  function makelist(){
-    for (i=0; i<dicentries.length; i++) {
+  function makelist(entries){
+    $("#dictionarylist").html("");
+    for (i=0; i<entries.length; i++) {
       var classname="ui-li-static ui-body-inherit";
       if (i==0) {
         classname+=" ui-first-child";
-      }else if (i==dicentries.length-1) {
+      }else if (i==entries.length-1) {
         classname+=" ui-last-child";
       }
-      var h3=$("<h3></h3>").html(dicentries[i].kiswahili);
-      var pstrong=$("<p></p>").append("<strong></strong>").html(dicentries[i].english);
-
+      if (!isEnglish) {
+        var h3=$("<h3></h3>").html(entries[i].kiswahili);
+        var pstrong=$("<p></p>").append("<strong></strong>").html(entries[i].english);
+      }else {
+        var h3=$("<h3></h3>").html(entries[i].english);
+        var pstrong=$("<p></p>").append("<strong></strong>").html(entries[i].kiswahili);
+      }
       var audiv=$("<a></a>");
       audiv.addClass("ui-btn-right jqm-home audio");
-      audiv.attr("data-filename", "assets/audio/greetings/"+dicentries[i].audio);
+      audiv.attr("data-filename", "assets/audio/greetings/"+entries[i].audio);
       audiv.html("audio");
       var li=$("<li></li>");
       li.append(h3).append(pstrong).append(audiv);
       var p = $("<span></span>");
-      if (typeof(dicentries[i].notes))
-        p.html(dicentries[i].notes);
+      if (typeof(entries[i].notes))
+        p.html(entries[i].notes);
       else
         p.html("No additional notes");
       p.hide();
@@ -69,8 +69,25 @@ console.log("Dictionary loaded");
         $("#notes").popup("open");
       });
       li.addClass(classname);
-      li.attr("id", dicentries[i].english);
+      li.attr("id", entries[i].english);
       $("#dictionarylist").append(li);
     }
   }
+
+  $("#dictionarySearch").keyup(function() {
+    var searchQuery = $(this).val();
+    var searchResults = [];
+    var strSearch;
+
+    for(k=0; k<dicentries.length; k++) {
+      if(!isEnglish) {
+        if(dicentries[k].kiswahili.toString().substring(0, searchQuery.length) == searchQuery)
+          searchResults.push(dicentries[k]);
+      } else {
+        if(dicentries[k].english.toString().substring(0, searchQuery.length) == searchQuery)
+          searchResults.push(dicentries[k]);
+      }
+    }
+    makelist(searchResults);
+  });
 });
